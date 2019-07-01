649f3e37e76bf23165571d11de498d8ad7056db6
Andrew Lin <andrewlin1722@gmail.com>
2019-06-30 20:24:55 -0400

    test timer

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 import json
 import random
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-
-from appscheduler.schedulers.blocking import BlockingScheduler
 
 from flask import Flask, request
 
@@ -13,38 +13,46 @@ app = Flask(__name__)
 
 timer = BlockingScheduler()
 
-@app.route('/', methods=['POST'])
-def webhook():
-    data = request.get_json()
+# @app.route('/', methods=['POST'])
+# def webhook():
+#     data = request.get_json()
+#
+#     #We don't want to reply to ourselves!
+#     if data['name'] == 'Andrew Lin':
+#         msg = 'testing testing'
+#         send_message(msg)
+#
+#     return "ok", 200
 
-    #We don't want to reply to ourselves!
-    if data['name'] == 'Andrew Lin':
-        msg = 'testing testing'
-        send_message(msg)
+@timer.scheduled_job('interval', minutes=1)
+def testfunction():
+    print("This is a test")
 
-    return "ok", 200
+# @timer.scheduled_job('cron', day=6, hour=20, minute =1)
+# def scheduled_job():
+#     i = random.randint(1,5)
+#     if i == 1:
+#         msg = "Anyone wanna drink?"
+#     if i == 2:
+#         msg = "Beer Die?"
+#     if i == 3:
+#         msg = "I can't believe no one wants to drink with me rn"
+#     if i == 4:
+#         msg = "Got some wounded soldiers from last night if anyone wants to help me finish them"
+#     send_message(msg)
+#
+#     return "ok", 200
 
-@timer.scheduled_job('cron', day=6, hour=20, minute =1)
-def scheduled_job():
-    i = random.randint(1,5)
-    if i == 1:
-        msg = "Anyone wanna drink?"
-    if i == 2:
-        msg = "Beer Die?"
-    if i == 3:
-        msg = "I can't believe no one wants to drink with me rn"
-    if i == 4:
-        msg = "Got some wounded soldiers from last night if anyone wants to help me finish them"
-    send_message(msg)
+# def send_message(msg):
+#     url  = 'https://api.groupme.com/v3/bots/post'
+#
+#     data = {
+#           'bot_id' : os.getenv('GROUPME_BOT_ID'),
+#           'text'   : msg,
+#          }
+#     request = Request(url, urlencode(data).encode())
+#     json = urlopen(request).read().decode()
 
-    return "ok", 200
+timer.start()
 
-def send_message(msg):
-    url  = 'https://api.groupme.com/v3/bots/post'
 
-    data = {
-          'bot_id' : os.getenv('GROUPME_BOT_ID'),
-          'text'   : msg,
-         }
-    request = Request(url, urlencode(data).encode())
-    json = urlopen(request).read().decode()
