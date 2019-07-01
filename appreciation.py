70ce1b5c06856fe221ac6682917195b8c6f35f49
Andrew Lin <andrewlin1722@gmail.com>
2019-06-30 21:52:35 -0400

    test appreciation bot

--- appreciation.py
+++ appreciation.py
@@ -11,7 +11,34 @@ timer = BlockingScheduler()
 
 @timer.scheduled_job('interval', minutes=1)
 def test_function():
-    print("This is a test")
+    i = random.randint(1,5)
+    if i == 1:
+        msg = "Appreciate 2-ply toilet paper and charmin ultra-soft while you still can"
+    if i == 2:
+        msg = "Appreicate Oliver Schmickel"
+    if i == 3:
+        msg = "Appreciate the days of Luis Nicolao (Vote for the Coach and the King)"
+    if i == 4:
+        msg = "Appreicate the Red Rockets, may their names be never forgotten"
+    if i == 5:
+        msg = "Appreciate the times when we didn't have a 3pm game on lawnparties"
+    if i == 6:
+        msg = "Appreciate Nice Weather"
+    if i == 6:
+        msg = "Appreciate non-dhall food"
+    send_message(msg)
+
+    return "ok", 200
+
+def send_message(msg):
+    url  = 'https://api.groupme.com/v3/bots/post'
+
+    data = {
+          'bot_id' : os.getenv('GROUPME_BOT_ID'),
+          'text'   : msg,
+         }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
 
 # @timer.scheduled_job('cron', day=6, hour=20, minute =1)
 # def scheduled_job():
